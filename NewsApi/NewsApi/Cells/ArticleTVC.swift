eeae72119955737fc186fb648391780820043d4e
Somnath <88436267+somnathjadhav-apple@users.noreply.github.com>
2022-07-22 19:10:17 +0530

    Add files via upload

---
+++ NewsApi/NewsApi/Cells/ArticleTVC.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTVC.swift
+//  NewsApi
+//
+//  Created by Somnath Jadhav on 7/21/22.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTVC : UITableViewCell {
+    
+    @IBOutlet weak var titleLabel : UILabel!
+    @IBOutlet weak var abstractLabel : UILabel!
+    @IBOutlet weak var articleImage: UIImageView!
+}
